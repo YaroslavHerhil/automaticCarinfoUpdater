f777c00c54e59445a039d344c5f4fccb212a2fdd
Yaro <yarik.342.ua.itstep@gmail.com>
2026-06-05 14:17:07 +0200

    tested stuff

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 CARAPI_URL =  os.environ["CARAPI_URL"]
 
 
+
+
+# carurl = "https://a1.gpsguard.eu/api/v1/vehicles/group/ZOJO"
+# webhookurl = "https://globalee.odoo.com/web/hook/7d027698-c4ee-4cbc-9fd2-41a5676ac42f"
+
 basic = HTTPBasicAuth(USERNAME, PASSWORD)
 
 
@@ -17,13 +22,9 @@ allcars = requests.get(CARAPI_URL, auth=basic).json()
 
 for car in allcars:
     payload = {
-        "jsonrpc": "2.0",
-        "method": "call",
-        "params": {
-            "lat": car["LastPosition"]["Latitude"],
-            "lng": car["LastPosition"]["Longitude"],
-            "license_plate": car["SPZ"],
-        }
+        "lat": float(car["LastPosition"]["Latitude"]),
+        "lng": float(car["LastPosition"]["Longitude"]),
+        "license_plate": car["SPZ"],
     }
     print(payload)
     response = requests.post(
@@ -33,3 +34,5 @@ for car in allcars:
     )
     print("Status Code", response.status_code)
     print("Response", response.text)
+    
+    #1AH B685
