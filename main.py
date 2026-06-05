1f7c967c7ed50d97d8477c07525743988cb5b0bc
Yaro <yarik.342.ua.itstep@gmail.com>
2026-06-05 12:29:03 +0200

    added workflow huh

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+from requests.auth import HTTPBasicAuth
+import requests
+import os
+from requests.auth import HTTPBasicAuth
+
+USERNAME = os.environ["API_USERNAME"]
+PASSWORD = os.environ["API_PASSWORD"]
+WEBHOOK_URL = os.environ["WEBHOOK_URL"]
+CARAPI_URL =  os.environ["CARAPI_URL"]
+
+
+basic = HTTPBasicAuth(USERNAME, PASSWORD)
+
+
+allcars = requests.get(CARAPI_URL, auth=basic).json()
+
+for car in allcars:
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "call",
+        "params": {
+            "lat": car["LastPosition"]["Latitude"],
+            "lng": car["LastPosition"]["Longitude"],
+            "license_plate": car["SPZ"],
+        }
+    }
+    print(payload)
+    response = requests.post(
+        WEBHOOK_URL,
+        json=payload,
+        headers={"Content-Type": "application/json"}
+    )
+    print("Status Code", response.status_code)
+    print("Response", response.text)
